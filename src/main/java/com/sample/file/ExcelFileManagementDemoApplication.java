c86fbbfb40363faa9fb128a81f2da63321f80bc6
SharmilaMohanraj <106313092+SharmilaMohanraj@users.noreply.github.com>
2022-07-31 17:50:56 +0530

    first commit

---
+++ src/main/java/com/sample/file/ExcelFileManagementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sample.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelFileManagementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelFileManagementDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sample/file/service/ExcelFileService.java
@@ -0,0 +1,22 @@
+package com.sample.file.service;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sample.file.model.ExcelFileAttachment;
+import com.sample.file.model.ExcelRecords;
+
+public interface ExcelFileService {
+
+	ExcelFileAttachment uploadExcelFileToDB(MultipartFile file, String filepath);
+
+	List<ExcelFileAttachment> getAllUploadedExcelFiles();
+
+	List<ExcelRecords> reviewRecordsOfExcel(Long fileId);
+
+	ExcelFileAttachment geExcelFileById(Long excelId);
+
+	void deleteExcelFile(Long fileId);
+
+}
